adff2016a993db752f5d25fc6e434bdbe56123b9
yoda <takenao.yoda@gmail.com>
2018-02-11 04:21:39 +0900

    Remove dependency.

--- src/android/WebViewActivity.java
+++ src/android/WebViewActivity.java
@@ -1,6 +1,7 @@
 package ore.cordova.modalwebview;
 
 import android.animation.ObjectAnimator;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -8,9 +9,6 @@ import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
 import android.os.Bundle;
-import android.support.annotation.ColorInt;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,10 +28,12 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.androidadvance.topsnackbar.TSnackbar;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class WebViewActivity extends AppCompatActivity {
+public class WebViewActivity extends Activity {
 
   private static final String EXTRA_URL = WebViewActivity.class.getName() + "#URL";
   private static final String EXTRA_TITLE = WebViewActivity.class.getName() + "#TITLE";
@@ -41,7 +41,7 @@ public class WebViewActivity extends AppCompatActivity {
   private static final String EXTRA_ERROR_BACKGROUND_COLOR = WebViewActivity.class.getName() + "#ERROR_BACKGROUND_COLOR";
 
   public static Intent newCallingIntent(Context context, String url, String title,
-                                        @ColorInt int errorTextColor, @ColorInt int errorBackgroundColor) {
+                                        int errorTextColor, int errorBackgroundColor) {
     return new Intent(context, WebViewActivity.class)
         .putExtra(EXTRA_URL, url)
         .putExtra(EXTRA_TITLE, title)
@@ -137,9 +137,7 @@ public class WebViewActivity extends AppCompatActivity {
   private static class MWVWebViewClient extends WebViewClient {
     private final Context context;
     private final ViewGroup containerView;
-    @ColorInt
     private final int errorTextColor;
-    @ColorInt
     private final int errorBackgroundColor;
 
     MWVWebViewClient(Context context, ViewGroup containerView, int errorTextColor, int errorBackgroundColor) {
@@ -173,7 +171,7 @@ public class WebViewActivity extends AppCompatActivity {
     private void handleError(final WebView webView) {
       final int messageId = ResourceUtils.getStringResourceIdentifier(context, "modalwebview_error");
       final int actionId = ResourceUtils.getStringResourceIdentifier(context, "modalwebview_action_reload");
-      Snackbar snackbar = Snackbar.make(containerView, messageId, Snackbar.LENGTH_LONG)
+      TSnackbar snackbar = TSnackbar.make(containerView, messageId, TSnackbar.LENGTH_LONG)
           .setAction(actionId, new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -182,7 +180,7 @@ public class WebViewActivity extends AppCompatActivity {
           });
       View snackbarView = snackbar.getView();
       if (this.errorTextColor != Color.TRANSPARENT) {
-        TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+        TextView textView = (TextView) snackbarView.findViewById(com.androidadvance.topsnackbar.R.id.snackbar_text);
         textView.setTextColor(this.errorTextColor);
       }
       if (this.errorBackgroundColor != Color.TRANSPARENT) {
@@ -193,8 +191,11 @@ public class WebViewActivity extends AppCompatActivity {
 
     private static boolean isForMainFrame(WebResourceRequest request) {
       try {
-        Method m = WebResourceRequest.class.getMethod("isForMainFrame");
-        return (Boolean)m.invoke(request);
+        Class<?> wrrClass = Class.forName("android.webkit.WebResourceRequest");
+        Method m = wrrClass.getMethod("isForMainFrame");
+        return (Boolean) m.invoke(request);
+      } catch (ClassNotFoundException e) {
+        return false;
       } catch (NoSuchMethodException e) {
         return false;
       } catch (InvocationTargetException e) {
